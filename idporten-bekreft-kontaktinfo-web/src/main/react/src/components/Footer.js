dcff7e3c037a8046cb3dea4ade52b997199c604b
Andreas Balevik <andreas.balevik@digdir.no>
2020-12-02 13:50:57 +0000

    2020-12-02-1350-fe607ea|PBLEID-21410: Fortsatt mangler ifm på språkvalg satt i eid-selector

--- idporten-bekreft-kontaktinfo-web/src/main/react/src/components/Footer.js
+++ idporten-bekreft-kontaktinfo-web/src/main/react/src/components/Footer.js
@@ -4,26 +4,29 @@ import {Trans, withTranslation} from "react-i18next";
 class Footer extends Component {
 
     render () {
+
+        const { t } = this.props;
+
         return (
             <footer className="f-Main">
                 <div className="f-Main_Content">
                     <div className="f-Main_Logo" aria-hidden="true"/>
                     <div className="f-Main_Info">
                         <a className="f-Main_Link"
-                           href="https://eid-systest-web01.dmz.local/opensso/support.jsp?service=MinIDChain&amp;pageid=dc5">
+                           href={t('url.contact-form-url')}>
                             <Trans i18nKey="misc.contact-form"/>
                         </a>
                         <a href="tel:+4780030300" className="f-Main_Link"><span><Trans i18nKey="misc.phone"/>: 800 30 300</span></a>
-                        <a target="_blank" className="f-Main_Link" href="https://eid.difi.no/nb/id-porten">
+                        <a target="_blank" className="f-Main_Link" href={t('url.login-help-url')}>
                             <Trans i18nKey="misc.login-help"/>
                         </a>
-                        <a target="_blank" className="f-Main_Link" href="https://eid.difi.no/nb/sikkerhet-og-personvern">
+                        <a target="_blank" className="f-Main_Link" href={t('url.security-and-privacy-url')}>
                             <Trans i18nKey="misc.security-and-privacy"/>
                         </a>
                         <p>
-                            <Trans i18nKey="misc.provided-by"/>
+
                             <a target="_blank" id="difilink" href="http://www.digdir.no/"
-                               className="f-Main_Link">Digitaliseringsdirektoratet</a>
+                               className="f-Main_Link"><Trans i18nKey="misc.provided-by"/></a>
                         </p>
                     </div>
                 </div>
