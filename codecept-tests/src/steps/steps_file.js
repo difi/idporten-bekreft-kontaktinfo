0e332ce3d3fe1157e061824b3aa0d87e306a23da
linesh raghawan <linesh.raghawan@digdir.no>
2020-12-17 07:36:36 +0100

    PBLEID-21521 ny test for tippsider og opprette KRR

--- codecept-tests/src/steps/steps_file.js
+++ codecept-tests/src/steps/steps_file.js
@@ -18,17 +18,6 @@ module.exports = function() {
       this.see("PIN-KODE");
       this.fillField('#input_PINCODE1_IDPORTEN', process.env.IBK_CODE);
       this.click('#nextbtn');
-
-      //this.see("Informasjonen nedenfor lagres i et felles kontaktregister som stat og kommune skal bruke nÃ¥r de kontakter deg");
-
-      // may ask to CREATE kontaktinfo
-      // this.tryTo(() => I.click('//*[@id="confirmContactinfo"]/div[5]/button')); // id missing - using xpath
-     // this.tryTo(() => I.click('//*[@id="idporten.inputbutton.CONTINUE"]'));
-
-      // may ask to CONFIRM kontaktinfo
-
-
-
     },
     createKRR(){
       this.see('DINE KONTAKTOPPLYSNINGER');
@@ -36,8 +25,8 @@ module.exports = function() {
       this.see('Informasjonen nedenfor lagres i et felles kontaktregister som stat og kommune skal bruke når de kontakter deg.');
       this.see('Du kan velge å gå videre uten å legge inn kontaktopplysninger.');
 
-      this.fillField('#idporten.input.CONTACTINFO_EMAIL', process.env.IBK_USERNAME+'@digdir.no');
-      this.fillField('#idporten.inputrepeat.CONTACTINFO_EMAIL', process.env.IBK_USERNAME+'@digdir.no');
+      this.fillField('#idporten.input.CONTACTINFO_EMAIL', '24079497513-Digdir@digdir.no');
+      this.fillField('#idporten.inputrepeat.CONTACTINFO_EMAIL', '24079497513-Digdir@digdir.no');
       this.fillField('#idporten.input.CONTACTINFO_MOBILE', '+4799999999');
       this.fillField('#idporten.inputrepeat.CONTACTINFO_MOBILE', '+4799999999');
       this.fillField('#input_PASSWORD_IDPORTEN', process.env.IBK_PASSWORD);
@@ -46,12 +35,12 @@ module.exports = function() {
     createKRRWithoutEmail(){
       this.see('DINE KONTAKTOPPLYSNINGER');
       this.see('Informasjonen nedenfor lagres i et felles kontaktregister som stat og kommune skal bruke når de kontakter deg.');
-      I.click('#idporten.inputbutton.CONTINUE_CONFIRM');
+      this.click('#idporten.inputbutton.CONTINUE_CONFIRM');
     },
     createKRRWithoutMobile(){
       this.see('DINE KONTAKTOPPLYSNINGER');
       this.see('Informasjonen nedenfor lagres i et felles kontaktregister som stat og kommune skal bruke når de kontakter deg.');
-      I.click('#idporten.inputbutton.CONTINUE_CONFIRM');
+      this.click('#idporten.inputbutton.CONTINUE_CONFIRM');
     },
   });
 };
